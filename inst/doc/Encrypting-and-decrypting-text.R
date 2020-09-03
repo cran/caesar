cf80c0112b2b019850d5f83f2321446df28230ae
Jacob Kaplan <jkkaplan6@gmail.com>
2020-09-03 12:52:11 +0000

    version 1.1.0

--- inst/doc/Encrypting-and-decrypting-text.R
+++ inst/doc/Encrypting-and-decrypting-text.R
@@ -1,31 +1,34 @@
-## ----echo=TRUE-----------------------------------------------------------
+## ----echo=TRUE----------------------------------------------------------------
  caesar::caesar(text = "As a rule, men worry more about what they can't see than about what they can.")
 
-## ----echo=TRUE-----------------------------------------------------------
+## ----echo=TRUE----------------------------------------------------------------
+ caesar::caesar(text = c("As a rule, men worry more about what they can't see than about what they can.", "The best way of avenging thyself is not to become like the wrong doer."))
+
+## ----echo=TRUE----------------------------------------------------------------
  caesar::caesar(text = "As a rule, men worry more about what they can't see than about what they can.", 
         shift = -100)
 
-## ----echo=TRUE-----------------------------------------------------------
+## ----echo=TRUE----------------------------------------------------------------
  caesar::caesar(text = "Dvcdcuxoh cphqczruuBcpruhcderxwczkdwcwkhBcfdq,wcvhhcwkdqcderxwczkdwcwkhBcfdqa",
         decrypt = TRUE)
 
-## ----echo=TRUE-----------------------------------------------------------
+## ----echo=TRUE----------------------------------------------------------------
  caesar::caesar(text = "rj};}ilc>[}d>e}nfiip}dfi>};:flk}n/;k}k/>p}';e=k}j>>}k/;e};:flk}n/;k}k/>p}';e]", 
         shift = -100, 
         decrypt = TRUE)
 
-## ----echo=TRUE-----------------------------------------------------------
+## ----echo=TRUE----------------------------------------------------------------
  caesar::seed_cipher(text = "As a rule, men worry more about what they can't see than about what they can.")
 
-## ----echo=TRUE-----------------------------------------------------------
+## ----echo=TRUE----------------------------------------------------------------
  caesar::seed_cipher(text = "As a rule, men worry more about what they can't see than about what they can.", 
         seed = -100)
 
-## ----echo=TRUE-----------------------------------------------------------
+## ----echo=TRUE----------------------------------------------------------------
  caesar::seed_cipher(text = "R#GdG<VfhbG$hXGeC<<oG$C<hGd+CV/Ge0d/G/0hoGdXg/G#hhG/0dXGd+CV/Ge0d/G/0hoG dX_",
         decrypt = TRUE)
 
-## ----echo=TRUE-----------------------------------------------------------
+## ----echo=TRUE----------------------------------------------------------------
  caesar::seed_cipher(text = "Eucfc{%>Dc-Dkc8S{{$c-S{DcfyS%*c8Qf*c*QD$cofkR*cuDDc*QfkcfyS%*c8Qf*c*QD$cofki", 
         seed = -100, 
         decrypt = TRUE)
